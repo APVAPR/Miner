3092c02fa4df5cf22b8ac3f4efda18eeb404a5b0
Sunnypyonk <sasha.pozniak@gmail.com>
2021-09-16 11:14:43 +0300

    No working

--- miner.py
+++ miner.py
@@ -1,6 +1,18 @@
 import tkinter as tk
 
 
+class MyButton(tk.Button):
+
+    def __init__(self, master, x, y, *args, **kwargs):
+        super(MyButton, self).__init__(master, *args, **kwargs)
+        self.x = x
+        self.y = y
+        self.is_mine = False
+
+    def __repr__(self):
+        return 'But'
+
+
 class MineSweeper:
     window = tk.Tk()
     ROW = 10
@@ -15,7 +27,7 @@ class MineSweeper:
         for i in range(MineSweeper.ROW):
             temp = []
             for j in range(MineSweeper.COLUMNS):
-                btn = tk.Button(MineSweeper.window, width=3, font='Calibry 15 bold')
+                btn = MyButton(MineSweeper.window, width=3, font='Clibry 15 bold')
                 temp.append(btn)
             self.buttons.append(temp)
 
@@ -28,15 +40,19 @@ class MineSweeper:
                 btn = self.buttons[i][j]
                 btn.grid(row=i, column=j)
 
+    def print_buttons(self):
+        for row_btn in self.buttons:
+            print(row_btn)
+
     def start(self):
         """
         Метод запуска основного цикла игры
         """
-        MineSweeper.window.mainloop()
+        self.create_wigets()
+        self.print_buttons()
+        game.window.mainloop()
 
 
 game = MineSweeper()
-game.create_wigets()
+
 game.start()
-for row_btn in game.buttons:
-    print(row_btn)
